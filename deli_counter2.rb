3e2fe8c72d7cf5652ea090652b5cf7d90966aea1
David Strauch <dmaster18@gmail.com>
2019-07-25 15:43:50 +0000

    Automatically backed up by Learn

--- deli_counter2.rb
+++ deli_counter2.rb
@@ -1,40 +1,43 @@
 # Write your code here.
-katz_deli = ["1. John", "2. Joe", "3. Rob"]
+katz_deli = ["John", "Joe", "Rob"]
+ordered_katz_deli = []
 
 def take_a_number(katz_deli, new_customer)
   index = katz_deli.length + 1
-  print "Welcome, #{new_customer}. You are number #{index} in line."
+  puts "Welcome, #{new_customer}. You are number #{index} in line."
   katz_deli.push(new_customer)
 end
 
-def line (katz_deli)
+def line (katz_deli, ordered_katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else
+    index = 1
     katz_deli.each do |customer|
-      index = 1 
-      customer = "#{index}. #{new_customer}"
+      customer = index.to_s << ". " << customer.to_s 
+      ordered_katz_deli.push(customer)
       index +=1
     end
-    print "The line is currently serving: #{katz_deli}."
+    puts "The line is currently serving: #{ordered_katz_deli}."
   end
 end
 
-def now_serving(katz_deli)
+def now_serving(katz_deli, ordered_katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
     front_of_the_line = katz_deli.shift
-    puts "Currently serving #{front_of_the_line.slice(3..(front_of_the_line.length-1))}"
+    ordered_katz_deli.shift
+    puts "Currently serving #{front_of_the_line}"
   end
 end
 
 take_a_number(katz_deli, "Esteban")
-line(katz_deli)
+line(katz_deli, ordered_katz_deli)
 take_a_number(katz_deli, "Esteban")
-line(katz_deli)
-now_serving(katz_deli)
-now_serving(katz_deli)
-now_serving(katz_deli)
-now_serving(katz_deli)
-line(katz_deli)
+line(katz_deli,ordered_katz_deli)
+now_serving(katz_deli, ordered_katz_deli)
+now_serving(katz_deli, ordered_katz_deli)
+now_serving(katz_deli, ordered_katz_deli)
+now_serving(katz_deli, ordered_katz_deli)
+line(katz_deli,ordered_katz_deli)
